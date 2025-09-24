229bbaa709cacecd3fbe02e9f303202e45692136
Çağrı Tükenmez <grfspear@gmail.com>
2025-09-24 18:46:21 +0300

    1

--- generator_web/obj/Debug/net8.0/generator_web.AssemblyInfo.cs
+++ generator_web/obj/Debug/net8.0/generator_web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("generator_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a45b9ec881413effbe091707caeffd516192d23")]
 [assembly: System.Reflection.AssemblyProductAttribute("generator_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("generator_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
